ed26df5f37ea6858de3fc7fd9cd091598ac63274
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-08-30 06:28:18 +0600

    User model schema.

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({
+  timestamps: true,
+})
+export class User extends Document {
+  @Prop({
+    type: String,
+    minlength: 6,
+    maxlength: 255,
+    required: [true, 'displayName field required'],
+  })
+  displayName: string;
+
+  @Prop({
+    unique: [true, 'Duplicate email entered'],
+    required: [true, 'email field required'],
+  })
+  email: string;
+
+  @Prop({
+    required: [true, 'password field required'],
+    minlength: 6,
+    maxlength: 1024,
+  })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
