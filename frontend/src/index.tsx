0e0c47b033bec1382ff8f0fa46346b8cae1bbf58
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-09-02 11:54:44 +0600

    Main component of the react app. Included redux, react-router and mui. Added interceptor, token for header of the request.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { persistor, store } from './app/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { addInterceptors } from './axios';
+import DialogsProvider from './components/Dialogs/DialogsProvider';
+
+addInterceptors(store);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <DialogsProvider>
+          <App />
+        </DialogsProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
