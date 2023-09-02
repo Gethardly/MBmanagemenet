5ed11ad4995986c245440e0dfd25452e73ce7aeb
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-09-02 12:26:15 +0600

    #FIX.

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,9 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 import { RootState } from './app/store';
 import { Store } from '@reduxjs/toolkit';
-import { configDotenv } from 'dotenv';
 
-configDotenv();
 const axiosApi = axios.create({
   baseURL: process.env.API,
 });
@@ -12,7 +10,7 @@ export const addInterceptors = (store: Store<RootState>) => {
   axiosApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = store.getState().users.user?.token;
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   });
