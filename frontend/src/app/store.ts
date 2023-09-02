aa5989d42a1eca75f7eecba4ddf1a0ce9420fa70
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-09-02 11:55:17 +0600

    Redux storage.

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,32 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { usersReducer } from '../features/users/usersSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, } from 'redux-persist/es/constants';
+import { notesReducer } from '../features/notes/notesSlice';
+
+const usersPersistConfig = {
+  key: "NOTES:users",
+  storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  users: persistReducer(usersPersistConfig, usersReducer),
+  notes: notesReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
