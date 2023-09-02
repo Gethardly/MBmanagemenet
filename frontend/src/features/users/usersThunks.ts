46252a4a2b5ec41a0e7223c763173bbeadd2126d
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-09-02 11:51:14 +0600

    All async request functions for users to api here.

---
+++ frontend/src/features/users/usersThunks.ts
@@ -0,0 +1,84 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { GlobalError, LoginMutation, User, UserMutation, ValidationError, } from '../../types';
+import axiosApi from '../../axios';
+import { isAxiosError } from 'axios';
+import { setUser } from './usersSlice';
+import { AppDispatch, RootState } from '../../app/store';
+
+export function handleAxiosError(e: any, rejectWithValue: any) {
+  if (isAxiosError(e) && e.response) {
+    if (e.response.status === 400 || e.response.status === 422) {
+      return rejectWithValue(e.response.data as ValidationError);
+    }
+  }
+
+  throw e;
+}
+
+export const login = createAsyncThunk<
+  User,
+  LoginMutation,
+  { rejectValue: GlobalError }
+>("users/login", async (loginMutation, { rejectWithValue }) => {
+  try {
+    const response = await axiosApi.post<User>("/login", loginMutation);
+    return response.data;
+  } catch (e) {
+    if (isAxiosError(e) && e.response && e.response.status === 401)
+      return rejectWithValue(e.response.data as GlobalError);
+
+    throw e;
+  }
+});
+
+export const createUser = createAsyncThunk<
+  void,
+  UserMutation,
+  { rejectValue: ValidationError }
+>("users/create", async (registerMutation, { rejectWithValue }) => {
+  try {
+    await axiosApi.post<User>("/signup", registerMutation);
+  } catch (e) {
+    handleAxiosError(e, rejectWithValue);
+  }
+});
+
+export const getEditingUser = createAsyncThunk<UserMutation, string>(
+  "users/getOne",
+  async (id: string) => {
+    try {
+      const response = await axiosApi.get<User>("/users/" + id);
+      const { email, displayName } = response.data;
+      return { email, displayName, password: "" };
+    } catch (e) {
+      throw new Error("Not found!");
+    }
+  }
+);
+
+interface UpdateUserParams {
+  id: string;
+  user: UserMutation;
+}
+
+export const updateUser = createAsyncThunk<
+  void,
+  UpdateUserParams,
+  { rejectValue: ValidationError; dispatch: AppDispatch; state: RootState }
+>("users/editOne", async (params, { rejectWithValue, dispatch, getState }) => {
+  try {
+    const currentUser = getState().users.user;
+    const response = await axiosApi.put(
+      "/change-user/" + params.id,
+      params.user
+    );
+    if (currentUser && currentUser._id === params.id) {
+      dispatch(setUser(response.data));
+    }
+  } catch (e) {
+    if (isAxiosError(e) && e.response && e.response.status === 400) {
+      return rejectWithValue(e.response.data as ValidationError);
+    }
+    throw e;
+  }
+});
