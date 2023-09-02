a5817f63b28870461bc07b50d6ae897b3a33b70d
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-09-02 11:50:26 +0600

    Apptoolbar with user menu for edit user and logout.

---
+++ frontend/src/components/AppToolbar/UserMenu.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useNavigate } from 'react-router-dom';
+import { User, UserMutation } from '../../types';
+import { Button, Divider, Menu, MenuItem, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { getEditingUser, updateUser } from '../../features/users/usersThunks';
+import ModalBody from '../ModalBody';
+import UserForm from '../../features/users/components/UserForm';
+import {
+  selectEditingError,
+  selectEditOneUserLoading,
+  selectOneEditingUser,
+  unsetUser,
+} from '../../features/users/usersSlice';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import useConfirm from '../Dialogs/Confirm/useConfirm';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+
+dayjs.locale("ru");
+
+interface Props {
+  user: User;
+}
+
+const UserMenu: React.FC<Props> = ({ user }) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const editingUser = useAppSelector(selectOneEditingUser);
+  const editLoading = useAppSelector(selectEditOneUserLoading);
+  const error = useAppSelector(selectEditingError);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { confirm } = useConfirm();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openDialog = async () => {
+    handleClose();
+    await dispatch(getEditingUser(user._id));
+    setIsDialogOpen(true);
+  };
+
+  const onFormSubmit = async (userToChange: UserMutation) => {
+    try {
+      await dispatch(updateUser({ id: user._id, user: userToChange })).unwrap();
+      setIsDialogOpen(false);
+    } catch (error) {
+      throw new Error(`Произошла ошибка: ${error}`);
+    }
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleClick}
+        color="inherit"
+        style={{ textTransform: "inherit" }}
+      >
+        <Typography mr={1}>{user.displayName}</Typography>
+        <AccountCircleIcon />
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={openDialog}>
+          <AccountBoxIcon sx={{ mr: 1 }} />
+          Редактировать профиль
+        </MenuItem>
+        <Divider key="user-divider" />
+        <MenuItem
+          sx={{ justifyContent: "center" }}
+          onClick={async () => {
+            if (
+              await confirm(
+                "Выход",
+                "Вы действительно хотите выйти? Так быстро?"
+              )
+            ) {
+              dispatch(unsetUser());
+              handleClose();
+              navigate("/");
+            }
+          }}
+        >
+          Выйти
+          <LogoutIcon sx={{ ml: 1 }} />
+        </MenuItem>
+      </Menu>
+      {editingUser && (
+        <ModalBody isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+          <UserForm
+            error={error}
+            onSubmit={onFormSubmit}
+            existingUser={editingUser}
+            isEdit
+            isLoading={editLoading}
+          />
+        </ModalBody>
+      )}
+    </>
+  );
+};
+
+export default UserMenu;
