9efeab99b9fe792f8d02bd7e25ad45b6ddcfafe7
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2024-09-08 16:54:33 +0600

    ci/cd fix test

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <Grid container spacing={1}>
+      Тест деплой
       <Grid item xs={2}>
         <Link to="/payment">
           <Button variant="contained">Пополнение</Button>
